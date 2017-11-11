b29379b66ac6fb23324c133788e436df42e9b7ee
VeyMess <rak200@mail.ru>
2017-11-11 15:02:09 +0300

    Add files via upload

---
+++ Server Code and Config/PhotonServer.cs
@@ -0,0 +1,29 @@
+﻿using Photon.SocketServer;
+using System.IO;
+using log4net.Config;
+using ExitGames.Logging.Log4Net;
+using ExitGames.Logging;
+
+namespace PhotonIntro
+{
+    public class PhotonServer : ApplicationBase
+    {
+        private readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            Log.Debug("PeerCreate");
+            return new UnityClient(initRequest);
+        }
+
+
+        protected override void Setup()
+        {
+            
+        }
+
+        protected override void TearDown()
+        {
+        }
+    }
+}
---
+++ Server Code and Config/UnityClient.cs
@@ -0,0 +1,93 @@
+﻿using Photon.SocketServer;
+using ExitGames.Logging;
+using PhotonHostRuntimeInterfaces;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace PhotonIntro
+{
+    public class UnityClient : ClientPeer
+    {
+        Thread tim;
+        float playerHealth;
+        float enemyHealth;
+
+        float playerDMG = 15f;
+        float enemyDMG = 15f;
+
+        private readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        public UnityClient(InitRequest initRequest):base(initRequest)
+        {
+            tim = new Thread(()=>
+            {
+                while (true)
+                {
+                    Thread.Sleep(120000);
+                    SendEvent(new EventData(0), new SendParameters());
+                }
+            });
+
+            tim.IsBackground = true;
+            tim.Start();
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            tim.Abort();
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            //Начало боя инициализация жизни обьектов = operation 1
+            if (operationRequest.OperationCode == (byte) 1)
+            {
+                playerHealth = 100;
+                enemyHealth = 100;
+
+                OperationResponse tempResp = new OperationResponse(operationRequest.OperationCode);
+                Dictionary<byte, object> dic = new Dictionary<byte, object>();
+
+                dic.Add(0, playerHealth);
+                dic.Add(1, enemyHealth);
+                tempResp.Parameters = dic;
+
+                SendOperationResponse(tempResp, sendParameters);
+            }
+
+            //пользователь хочет атаковать = operation 2
+            else if (operationRequest.OperationCode == (byte) 2)
+            {
+                OperationResponse tempResponse = new OperationResponse(operationRequest.OperationCode);
+                Dictionary<byte, object> respDic = new Dictionary<byte, object>();
+                // враг мертв
+                if (enemyHealth == 0)
+                {
+                    respDic.Add(0, false);
+                }
+                else
+                {
+                    // у врага жизни больше чем сила атаки игрока
+                    if (enemyHealth > playerDMG)
+                    {
+                        enemyHealth -= playerDMG;
+                    }
+                    // у врага жизни меньше чем сила атаки игрока
+                    else
+                    {
+                        enemyHealth = 0;
+                    }
+                    respDic.Add(0, true);
+                }
+                respDic.Add(1, enemyHealth);
+                tempResponse.Parameters = respDic;
+
+                SendOperationResponse(tempResponse, sendParameters);
+            }
+            else
+            {
+                Log.Debug("Unknow operation");
+            }
+        }
+    }
+}
